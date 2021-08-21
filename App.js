e5cf17c97159362ef19961494721f61891796e74
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-08-21 12:18:56 +0530

    Added firebase Config

--- App.js
+++ App.js
@@ -5,8 +5,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Landing from "./components/auth/Landing";
 import Register from "./components/auth/Register";
+import firebase from "firebase/app";
 
 const Stack = createStackNavigator();
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATNDh1d2RmfUvflBtBpCtDRayBdDtxleI",
+  authDomain: "ultronic-insta-7a778.firebaseapp.com",
+  projectId: "ultronic-insta-7a778",
+  storageBucket: "ultronic-insta-7a778.appspot.com",
+  messagingSenderId: "158217758017",
+  appId: "1:158217758017:web:4c0a7147245325be686dc0",
+  measurementId: "G-YZDF5Y3BLN",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
