74ade9dfdea18d9f15c9c8822ea9e058b7d63c02
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-08-21 16:57:07 +0530

    Created auth state listener

--- App.js
+++ App.js
@@ -91,7 +91,7 @@ export class App extends Component {
 
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Loading</Text>
+        <Text>Logged</Text>
       </View>
     );
   }
