f6a7a13c0d5c9fe342b53a7cc60c891b8684d7a9
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-08-21 11:58:21 +0530

    Built the landing page

---
+++ components/auth/Landing.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { Button } from "react-native-elements";
+
+export default function Landing({ navigation }) {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
+      <Button
+        titleStyle={{ width: 100 }}
+        style={{
+          minWidth: 80,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          height: 50,
+          padding: 10,
+        }}
+        title="Register"
+        onPress={() => navigation.navigate("Register")}
+      />
+      <Button
+        title="Login"
+        titleStyle={{ width: 100 }}
+        style={{
+          minWidth: 80,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          height: 50,
+        }}
+        onPress={() => navigation.navigate("Login")}
+      />
+    </SafeAreaView>
+  );
+}
